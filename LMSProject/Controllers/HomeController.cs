f380d74e3dc0cca792842bfe3e77a762a974f218
nawal-ali <nmaaa3899@gmail.com>
2026-05-13 01:35:49 +0300

    added homeController

---
+++ LMSProject/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MLSCore.IdentityModel;
+
+namespace LMSProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
